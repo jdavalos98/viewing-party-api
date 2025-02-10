1a8453e49f5b9dad42218962702417fee5e7c1b5
Joel Davalos <jsdavalos@gmail.com>
2025-02-10 13:45:50 -0700

    Refactor: Added error handling in controller and moved movie model into poros directory

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::MoviesController < ApplicationController
   
   def top_rated
     movies = Movie.top_rated
-    render json: MovieSerializer.new(movies), status: :ok
+
+    if movies.nil?
+      render json: ErrorSerializer.format_error({ message: "Failed to fetch top-rated movies", status: 500 }), status: :internal_server_error
+    else
+      render json: MovieSerializer.new(movies), status: :ok
+    end
   end
 end
