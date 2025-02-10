461f3bbf5149bf6be1c068da1a525ac558384903
Joel Davalos <jsdavalos@gmail.com>
2025-02-10 16:50:59 -0700

    Refactor: Have passing tests for top rated movies

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::MoviesController < ApplicationController
-  
   def top_rated
-    movies = Movie.top_rated
-
-    if movies.nil?
-      render json: ErrorSerializer.format_error({ message: "Failed to fetch top-rated movies", status: 500 }), status: :internal_server_error
-    else
-      render json: MovieSerializer.new(movies), status: :ok
+    begin
+      movies = Movie.top_rated
+      render json: MovieSerializer.new(movies), status: :ok, content_type: "application/vnd.api+json"
+    rescue Faraday::ConnectionFailed
+      render json: { errors: [{ detail: "Service unavailable: Unable to reach The Movie DB API" }] }, status: :service_unavailable
+    rescue JSON::ParserError
+      render json: { errors: [{ detail: "Invalid response from Movie DB API" }] }, status: :internal_server_error
+    rescue StandardError => e
+      render json: { errors: [{ detail: e.message }] }, status: :internal_server_error
     end
   end
 end
--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,4 +1,4 @@
-class Movie 
+class Movie
   attr_reader :id, :title, :vote_average
 
   def initialize(data)
@@ -6,19 +6,23 @@ class Movie
     @title = data[:title]
     @vote_average = data[:vote_average]
   end
-  
+
   def self.top_rated
     response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated") do |req|
       req.params['api_key'] = ENV['MOVIE_DB_API_KEY']
     end
 
-    return nil unless response.success?
-
-    data = JSON.parse(response.body, symbolize_names: true)[:results].first(20)
-
-    data.map { |movie_data| Movie.new(movie_data) }
+    if response.success?
+      begin
+        data = JSON.parse(response.body, symbolize_names: true)[:results].first(20)
+        data.map { |movie_data| Movie.new(movie_data) }
+      rescue JSON::ParserError
+        []
+      end
+    else
+      []
+    end
   rescue Faraday::ConnectionFailed
-    nil
-  rescue JSON::ParserError
-    nil
+    raise StandardError.new("Network failure: Unable to reach The Movie DB API")
   end
+end
